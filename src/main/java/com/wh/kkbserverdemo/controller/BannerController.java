d79001c0645d055ed97dfa0a3c3743d88c059bcf
zhazhahehedian <zhazhahehedian@aliyun.com>
2021-12-26 11:19:30 +0800

    简单controller接口

--- src/main/java/com/wh/kkbserverdemo/controller/BannerController.java
+++ src/main/java/com/wh/kkbserverdemo/controller/BannerController.java
@@ -1,10 +1,12 @@
 package com.wh.kkbserverdemo.controller;
 
+import com.wh.kkbserverdemo.entity.Banner;
 import com.wh.kkbserverdemo.entity.Result;
 import com.wh.kkbserverdemo.service.BannerService;
 import com.wh.kkbserverdemo.vo.request.BannerRequest;
 import com.wh.kkbserverdemo.vo.response.BannerResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,28 @@ public class BannerController {
 
     @RequestMapping("/qryListByPage")
     public Result<?> qryListByPage(BannerRequest bannerRequest){
-        return null;
+        return Result.success(bannerService.qryListByPage(bannerRequest));
     }
+
+    @RequestMapping("/findById/{id}")
+    public Result<?> findById(@PathVariable(value = "id") Integer id){
+        return Result.success(bannerService.findById(id));
+    }
+
+    @RequestMapping("/update")
+    public Result<?> update(BannerResponse bannerResponse){
+        return Result.success(bannerService.updateBanner(bannerResponse));
+    }
+
+    @RequestMapping("/insert")
+    public Result<?> insert(Banner banner){
+        return Result.success(bannerService.insertBanner(banner));
+    }
+
+    @RequestMapping("/delete/{id}")
+    public Result<?> deleteById(@PathVariable(value = "id") Integer id){
+        return Result.success(bannerService.deleteById(id));
+    }
+
+
 }
